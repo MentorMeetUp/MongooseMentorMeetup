40708126fb50ae1417d5070850f1e231a7f8bf21
Phuoc-Viet Nguyen <sovietnguyen2291@gmail.com>
2017-10-03 11:18:56 -0700

    Card and Results changes

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,61 +1,36 @@
 import React from "react";
-import {Grid} from "react-bootstrap";
-import {Row} from "react-bootstrap";
-import {Col} from "react-bootstrap";
+// import {Grid} from "react-bootstrap";
+// import {Row} from "react-bootstrap";
+// import {Col} from "react-bootstrap";
 import {Thumbnail} from "react-bootstrap";
 import {Button} from "react-bootstrap";
 
 
-
 const Card = props => (
-  <Grid>
-    <Row>
-    <Col xs={6} md={4} lg={3}>
+  <div>
       <Thumbnail src={props.photo} alt="User Photo">
       <ul>
         <li>
           <strong>Name:</strong> {props.firstName} {props.lastName}
         </li>
         <li>
-          <strong>Skill:</strong> {props.skills}
+          <strong>Skill:</strong> {props.skills} + " "
         </li>
         <li>
           <strong>Location:</strong> {props.city}, {props.state}
         </li>
+        <li>
+          <strong>Cost:</strong> {props.cost1}
+        </li><br></br>
+        <li>
+          <strong>About Me:</strong> {props.aboutMe}. 
+        </li>
       </ul>
-        <p>
-          <Button bsStyle="primary" bsSize="xsmall">free</Button>   <Button bsStyle="primary" bsSize="xsmall">Button</Button>
-        </p>
+      <p>
+        <Button bsStyle="primary" bsSize="xsmall"><span className="glyphicon glyphicon-envelope">&nbsp;</span>{props.email}</Button>  
+      </p>
       </Thumbnail>
-    </Col>
-    </Row>
-  </Grid>
+  </div>
 );
-
-
-
-
-
-// const UserCard = props => (
-//   <div className="card">
-//     <div className="img-container">
-//       <img alt={props.name} src={props.image} />
-//     </div>
-//     <div className="content">
-//       <ul>
-//         <li>
-//           <h4>Name:</h4> {props.name}
-//         </li>
-//         <li>
-//           <strong>Occupation:</strong> {props.occupation}
-//         </li>
-//         <li>
-//           <strong>Location:</strong> {props.location}
-//         </li>
-//       </ul>
-//     </div>
-//   </div>
-    
-// );
 
 export default Card;
--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-import "./Results.css";
 import API from "../../utils/API";
 import Card from "../../components/Card";
+import {Grid} from "react-bootstrap";
+import {Row} from "react-bootstrap";
+import {Col} from "react-bootstrap";
+import "./cardStyle.css";
 
 class Results extends Component {
   state = {
@@ -19,12 +21,14 @@ class Results extends Component {
     .catch(err => console.log(err));
   };
 
-  	render() {
-	    return (
-			<div className="container">
+    render() {
+      return (
+        <Grid>
+          <Row>
               {this.state.skills.length ? (
                 <div>
                 {this.state.skills.map(result => (
+                  <Col xs={5} md={3} lg={3}>
                   <Card
                     key={result.id}
                     firstName={result.firstName}
@@ -33,39 +37,20 @@ class Results extends Component {
                     city={result.city}
                     state={result.state}
                     skills={result.skills} 
-                  />
+                    aboutMe={result.aboutMe}
+                    cost={result.cost}
+                    email={result.email}
+                  /></Col>
                 ))}
                 </div>
             ) : (
               <h3>No mentors for that skill yet!</h3>
-            )}
-			</div>                  
-		);
-	}
+            )}           
+          </Row>
+        </Grid>
+    );
+  }
 }
-
-
-
-// class Results extends Component {
-//   state = {
-//     skill: {}
-//   };
- 
-//   componentDidMount() {
-//     API.getUserSkill(this.props.match.params.skill)
-//       .then(res => console.log({ skill: res.data }))
-//       .catch(err => console.log(err));
-//   }
-  
-
-//   	render() {
-// 	    return (
-// 			<div className="container">
-// 			    <h1>Results Page  </h1>
-//           <Card />
-// 			</div>                  
-// 		);
-// 	}
-// }                 
+          
 
 export default Results;
